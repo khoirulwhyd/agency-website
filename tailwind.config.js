63117119b0d5effb3124bc5095678030a7a339c3
khoirulwhyd <gamingkenzo7@gmail.com>
2023-10-22 19:51:44 +0700

    add taos animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,15 +18,25 @@
 const withMT = require("@material-tailwind/html/utils/withMT");
 
 module.exports = withMT({
-  content: [
-    "./src/index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./node_modules/flowbite/**/*.js",
+  // content: [
+  //   "./src/index.html",
+  //       "./src/**/*.{js,ts,jsx,tsx}",
+  //       "./node_modules/flowbite/**/*.js",
+  // ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['./src/*.{html,js}', './node_modules/flowbite/**/*.js'],
+  },
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin', 'taos/plugin')
   ],
 });
